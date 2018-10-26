d589e803a8c0dad565cef20809bf149956b7e94e
markov-r <37890942+markov-r@users.noreply.github.com>
2018-10-26 13:53:55 +0200

    clear code

--- greedy/Jump Game LeetCode.java
+++ greedy/Jump Game LeetCode.java
@@ -8,56 +8,35 @@ public class Main {
 //        int[] nums = {2, 0, 9};
 //        int[] nums = {3, 0, 0, 3, 0, 1, 3, 0, 0, 0, 2};
 //        int[] nums = {};
+//        int[] nums = {1};
+//        int[] nums = {0, 2};
         int[] nums = {3, 0, 0, 5, 0, 0, 1, 0, 1};
-
         System.out.println(canJump(nums));
     }
 
-/* Zeroes are the main problem and need to be jumped over, so we find all clusters of zeroes in the array.
-    Then for each cluster check if it's possible to be jumped over (loop from cluster start backwards till array start(0)). */
+/* Zeroes are the main problem and need to be jumped over, so we find all zeroes in the array.
+    Then for each zero check if it's possible to be jumped over (loop from zero index minus one backwards till array start). */
 
     private static boolean canJump(int[] nums) {
-        // Fill the clusters of zeroes in a list of lists
-        List<List<Integer>> bigList = new ArrayList<>();
-        boolean inZeroGroup = false;
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == 0) {
-                inZeroGroup = true;
                 list.add(i);
-            } else if (inZeroGroup) {
-                bigList.add(list);
-                list = new ArrayList<>();
-                inZeroGroup = false;
             }
         }
-        if (list.size() > 0) {
-            bigList.add(list);
-        }
-        //Go from each zero cluster backwards till array start and check if it's possible to be jumped over
-        for (List<Integer> smList : bigList) {
-            if (!canJumpOver0s(nums, smList.get(0) - 1, smList.size())) {
+        for (int i : list) {
+            if (!canJumpOver0s(nums, i - 1)) {
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean canJumpOver0s(int[] nums, int from, int zeroCnt) {
+    private static boolean canJumpOver0s(int[] nums, int from) {
         int i, j;
         for (i = from, j = 0; i >= 0; i--, j++) {
-            if (nums[i] > zeroCnt + j) return true;
+            if (nums[i] > j + 1) return true;
         }
         return false;
-    }
-
-    private static void printList(List<List<Integer>> bigList) {
-        for (List<Integer> list : bigList) {
-            for (int i : list) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("----------");
     }
 }
