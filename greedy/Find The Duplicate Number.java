16784e5b80d59535ce9fa97f72e7fbee06ff2f66
markov-r <37890942+markov-r@users.noreply.github.com>
2018-11-14 21:58:09 +0200

    Find Duplicate task added

---
+++ greedy/Find The Duplicate Number.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+                    //0  1  2  3  4  5
+        int[] nums = {1, 4, 3, 2, 5, 2};
+        System.out.println(findDuplicate(nums));
+    }
+
+    private static int findDuplicate(int[] nums) {
+        int tortoise = nums[0];                 // Find the intersection point of the two runners.
+        int hare = nums[0];                     // Tortoise goes ahead one step, hare goes two
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        int ptr1 = nums[0];                     // Find the "entrance" to the cycle.
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+        return ptr1;
+    }
+}
