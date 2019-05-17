ace77512b5741526a4e5173db5c571f58413773d
markov-r <37890942+markov-r@users.noreply.github.com>
2019-05-17 11:23:57 +0300

    Update Counting Bits.java

--- dynamic programming/Counting Bits.java
+++ dynamic programming/Counting Bits.java
@@ -13,7 +13,7 @@ public class Main {
     /** When reaching a power of 2 (1 in result array) and
      * all digits up to here are n, the next x digits are old n ones, adding +1 to each
      * e.g. up till now ->  0 1 1 2
-     * next 4 digits are -> 1 2 2 3 */
+     * next 4 digits are -> 1 2 2 3. */
 
     private static int[] countBits(int num) {
         int dp[] = new int[num+1];
